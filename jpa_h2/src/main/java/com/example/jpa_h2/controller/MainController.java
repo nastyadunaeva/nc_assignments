58718b8d079e8d18eb3e30c5f1fd077556160fc6
nastyadunaeva <56322861+nastyadunaeva@users.noreply.github.com>
2021-06-27 23:44:51 +0300

    Add files via upload

--- jpa_h2/src/main/java/com/example/jpa_h2/controller/MainController.java
+++ jpa_h2/src/main/java/com/example/jpa_h2/controller/MainController.java
@@ -5,10 +5,12 @@ import com.example.jpa_h2.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class MainController {
@@ -16,7 +18,7 @@ public class MainController {
     @Autowired
     private PersonCrudRepository personCrudRepository;
  
-    @RequestMapping("/all")
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
     public String all() {
         Iterable<Person> all = personCrudRepository.findAll();
         StringBuilder sb = new StringBuilder();
@@ -24,7 +26,7 @@ public class MainController {
         return sb.toString();
     }
     
-    @RequestMapping("/{id}")
+    @GetMapping("/{id}")
     public String index(@PathVariable long id) {
     	Long i = new Long(id);
         Iterable<Person> all = personCrudRepository.findByIdLike(i);
@@ -33,7 +35,7 @@ public class MainController {
         return sb.toString();
     }
     
-    @RequestMapping("/name/{lastName}")
+    @GetMapping("/name/{lastName}")
     public String lastName(@PathVariable String lastName) {
         Iterable<Person> all = personCrudRepository.findByLastNameLike(lastName);
         StringBuilder sb = new StringBuilder();
